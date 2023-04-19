4e30b68b902f5ebb8765acc3647af04ed3921d3c
smiadv2022 <msorokina@spacemedia.com.ua>
2023-04-19 19:10:21 +0300

    Update API.js

--- src/js/API.js
+++ src/js/API.js
@@ -1,13 +1,14 @@
 const BASE_URL='https://restcountries.com/v3.1/name';
 const fields =`?feilds=name,capital,population,flags,languages`;
 
-export function fetchCountries (nameCountries){
+export function fetchCountries(nameCountries) {
   fetch(`${BASE_URL}/${nameCountries}${fields}`)
-  .then(data =>{
-    if (!data.ok ) {
-    throw new Error (data.status);
-    }
-     return data.json();
-});
+    .then(data => {
+      if (!data.ok) {
+        throw new Error(data.status);
+      }
+      return data.json();
+    });
+  
 }
 
